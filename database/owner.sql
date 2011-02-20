91a62422b7745428b2d41e974942efa917e69fac
cbarrett <christopher.r.barrett@gmail.com>
2011-02-20 18:48:06 -0500

    Modified sql

--- database/owner.sql
+++ database/owner.sql
@@ -1,1 +1,2 @@
-'William Grant', 'William Grant & Sons Distillers Limited', 'The Glenfiddich Distillery', 'http://www.scotchwhisky.net/profile/william_grant.htm'
+insert into owner
+(null, 'William Grant', 'William Grant & Sons Distillers Limited', 'The Glenfiddich Distillery', 'http://www.scotchwhisky.net/profile/william_grant.htm');
